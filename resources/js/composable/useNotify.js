00de4d1b143bc9402acf7b6d8617b318e4ea44a0
Hitalo Salucci <hitalo.salucci@hotmail.com>
2025-05-22 20:43:41 -0300

    FEAT: Ajustes notify global

--- resources/js/composable/useNotify.js
+++ resources/js/composable/useNotify.js
@@ -9,12 +9,11 @@ export default function useNotify() {
       html: true,
       classes: "bg-success-notify",
       position,
-      avatar: "/svg/verify-notify.svg",
       actions: [
         {
           icon: "close",
           color: "green-6",
-          round: true,
+          rounded: true,
           handler: () => {
             /* ... */
           },
@@ -29,12 +28,11 @@ export default function useNotify() {
       html: true,
       classes: "bg-error-notify",
       position,
-      avatar: "/svg/error-notify-round.svg",
       actions: [
         {
           icon: "close",
           color: "red-5",
-          round: true,
+          rounded: true,
           handler: () => {
             /* ... */
           },
@@ -49,33 +47,11 @@ export default function useNotify() {
       html: true,
       classes: "bg-warning-notify",
       position,
-      avatar: "/svg/notify-warning.svg",
       actions: [
         {
           icon: "close",
           color: "yellow-9",
-          round: true,
-          handler: () => {
-            /* ... */
-          },
-        },
-      ],
-    });
-  };
-
-  const notifyAlert = (message, position, icon) => {
-    $q.notify({
-      message: message,
-      html: true,
-      position,
-      classes: "sci-alert-notify",
-      icon: icon || "mood_bad",
-      textColor: "yellow-9",
-      actions: [
-        {
-          icon: "close",
-          color: "yellow-9",
-          round: true,
+          rounded: true,
           handler: () => {
             /* ... */
           },
@@ -90,12 +66,11 @@ export default function useNotify() {
       message: message,
       classes: "bg-info-notify",
       position,
-      avatar: "/svg/warning-info.svg",
       actions: [
         {
           icon: "close",
           color: "blue-5",
-          round: true,
+          rounded: true,
           handler: () => {
             /* ... */
           },
@@ -108,7 +83,6 @@ export default function useNotify() {
     notifySuccess,
     notifyError,
     notifyWarning,
-    notifyInfo,
-    notifyAlert,
+    notifyInfo
   };
 }
