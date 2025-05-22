d916083f167c161dd9bf64efbfc97b28997b7a6e
Hitalo Salucci <hitalo.salucci@hotmail.com>
2025-05-22 14:01:23 -0300

    FEAT: Change title principal page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel + Vue</title>
+    <title>Planifica App</title>
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
 <body>
