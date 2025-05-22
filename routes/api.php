45bb5d3444127a3c1e2d1508c497b46c944f1cc0
Hitalo Salucci <hitalo.salucci@hotmail.com>
2025-05-22 03:50:16 -0300

    Novas rotas base para o sistema

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\User\UserController;
 use App\Http\Controllers\Api\V1\Auth\AuthController;
+use App\Http\Controllers\Api\V1\Event\EventController;
+use App\Http\Controllers\Api\V1\Room\RoomController;
+use App\Http\Controllers\Api\V1\CoffeeRoom\CoffeeRoomController;
+use App\Http\Controllers\Api\V1\Person\PersonController;
 
 Route::prefix('v1')->group(function () { //Versionamento manual da rota
-
+    
     Route::prefix('auth')->group(function () {
         Route::post('/', [AuthController::class, 'login'])->name('login');
         Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
@@ -14,12 +18,46 @@ Route::prefix('v1')->group(function () { //Versionamento manual da rota
         Route::get('/', [AuthController::class, 'getLoggedUser'])->middleware('auth:sanctum')->name('auth.user');
     });
 
-    Route::prefix('users')->group(function () {
+    //As rotas abaixo estarão protegidas pelo middleware do sanctum, ou seja, precisa ter autenticação
+    
+    Route::prefix('users')->middleware('auth:sanctum')->group(function () {
         Route::get('/', [UserController::class, 'getAll']);
         Route::get('/{perPage}/{page?}', [UserController::class, 'getAllPaginated']);
         Route::post('/', [UserController::class, 'store']);
         Route::put('/{id}', [UserController::class, 'update']);
         Route::delete('/{id}', [UserController::class, 'destroy']);
+    });
+
+    Route::prefix('people')->middleware('auth:sanctum')->group(function () {
+        Route::get('/', [PersonController::class, 'getAll']);
+        Route::get('/{perPage}/{page?}', [PersonController::class, 'getAllPaginated']);
+        Route::post('/', [PersonController::class, 'store']);
+        Route::put('/{id}', [PersonController::class, 'update']);
+        Route::delete('/{id}', [PersonController::class, 'destroy']);
+    });
+
+    Route::prefix('events')->middleware('auth:sanctum')->group(function () {
+        Route::get('/', [EventController::class, 'getAll']);
+        Route::get('/{perPage}/{page?}', [EventController::class, 'getAllPaginated']);
+        Route::post('/', [EventController::class, 'store']);
+        Route::put('/{id}', [EventController::class, 'update']);
+        Route::delete('/{id}', [EventController::class, 'destroy']);
+    });
+
+    Route::prefix('rooms')->middleware('auth:sanctum')->group(function () {
+        Route::get('/', [RoomController::class, 'getAll']);
+        Route::get('/{perPage}/{page?}', [RoomController::class, 'getAllPaginated']);
+        Route::post('/', [RoomController::class, 'store']);
+        Route::put('/{id}', [RoomController::class, 'update']);
+        Route::delete('/{id}', [RoomController::class, 'destroy']);
+    });
+
+    Route::prefix('coffee-rooms')->middleware('auth:sanctum')->group(function () {
+        Route::get('/', [CoffeeRoomController::class, 'getAll']);
+        Route::get('/{perPage}/{page?}', [CoffeeRoomController::class, 'getAllPaginated']);
+        Route::post('/', [CoffeeRoomController::class, 'store']);
+        Route::put('/{id}', [CoffeeRoomController::class, 'update']);
+        Route::delete('/{id}', [CoffeeRoomController::class, 'destroy']);
     });
 
 });
