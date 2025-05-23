119bdd62cdd227687eb9bda31599a4858ba0d033
Hitalo Salucci <hitalo.salucci@hotmail.com>
2025-05-23 08:48:10 -0300

    FEAT: implements attachPerso,  deteachPerson e getPeople

--- app/Repositories/Event/EventRepository.php
+++ app/Repositories/Event/EventRepository.php
@@ -5,10 +5,66 @@ namespace App\Repositories\Event;
 use App\Models\Event\EventModel;
 use App\Repositories\BaseRepository;
 
+/**
+ * @extends BaseRepository<EventModel>
+ */
 class EventRepository extends BaseRepository
 {
     public function __construct(EventModel $model)
     {
         parent::__construct($model);
     }
+
+    /**
+     * Vincula uma pessoa a um evento
+     * 
+     * @param int $eventId ID do evento
+     * @param int $personId ID da pessoa
+     * @param string|null $entryCode Código de entrada (opcional)
+     * @return void
+     */
+    public function attachPerson(int $eventId, int $personId, ?string $entryCode = null)
+    {
+        $event = $this->getById($eventId);
+
+        $event->people()->attach($personId, ['entry_code' => $entryCode]);
+    }
+
+    /**
+     * Remove o vínculo de uma pessoa com um evento
+     * 
+     * @param int $eventId ID do evento
+     * @param int $personId ID da pessoa
+     * @return void
+     */
+    public function detachPerson(int $eventId, int $personId)
+    {
+        $event = $this->getById($eventId);
+        
+        if (!$event->people()->where('person_id', $personId)->exists()) {
+            throw new \Exception('Esta pessoa não está vinculada ao evento');
+        }
+        
+        $event->people()->detach($personId);
+    }
+
+    /**
+     * Retorna lista das pessoas no evento
+     *
+     * @param int $eventId
+     * @return array
+     */
+    public function getPeopleByEvent(int $eventId): array
+    {
+        $event = $this->model->with(['people' => function($query) {
+            $query->wherePivotNull('deleted_at'); // filtra apenas pivots não deletados
+        }])->find($eventId);
+
+        if (!$event) {
+            throw new \Exception("Evento não encontrado.");
+        }
+
+        return $event->people->toArray();
+    }
+
 }
--- app/Services/Event/EventService.php
+++ app/Services/Event/EventService.php
@@ -3,18 +3,64 @@
 namespace App\Services\Event;
 
 use App\DTOs\Event\CreateEventDTO;
+use App\DTOs\EventPerson\CreateEventPersonDTO;
+use App\DTOs\EventPerson\DetachEventPersonDTO;
 use App\Models\Event\EventModel;
 use App\Repositories\Event\EventRepository;
 use App\Services\BaseService;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @extends BaseService<EventModel, CreateEventDTO>
+ * @extends BaseService<EventModel, EventRepository>
  */
 class EventService extends BaseService
 {
+
     public function __construct(EventRepository $repository)
     {
         parent::__construct($repository);
+    }
+
+    /**
+     * Vincula uma pessoa a um evento.
+     *
+     * @param CreateEventPersonDTO $dto
+     * @return void
+     */
+    public function attachPerson(CreateEventPersonDTO $dto)
+    {  
+
+        /** @var EventRepository $repository */
+        $repository = $this->repository;
+        
+        $repository->attachPerson($dto->event_id, $dto->person_id, $dto->entry_code);
+    }
+
+    /**
+     * Remove o vínculo de uma pessoa com um evento
+     *
+     * @param DetachEventPersonDTO $dto
+     * @return void
+     */
+    public function detachPerson(DetachEventPersonDTO $dto)
+    {
+        /** @var EventRepository $repository */
+        $repository = $this->repository;
+        
+        $repository->detachPerson($dto->event_id, $dto->person_id);
+    }
+
+    /**
+     * Retorna lista das pessoas no evento
+     *
+     * @param int $eventId
+     * @return array
+     */
+    public function getPeopleByEvent(int $eventId): array
+    {   
+        /** @var EventRepository $repository */
+        $repository = $this->repository;
+
+        return $repository->getPeopleByEvent($eventId);
     }
 }
