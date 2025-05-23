b9046e51ffbf86a7f38451afb885f5a0b73aba99
Hitalo Salucci <hitalo.salucci@hotmail.com>
2025-05-23 05:01:09 -0300

    FEAT: Adicionando funcionalidade no botão para uso global

--- resources/js/components/inputs/StandardButton.vue
+++ resources/js/components/inputs/StandardButton.vue
@@ -12,8 +12,29 @@
         </span>
         <q-icon v-if="rightIcon" :name="rightIcon" :size="rightSizeIcon" />
       </div>
-      <q-spinner v-if="loading" size="24px" class="absolute" :class="classSpinner" />
+      <q-spinner v-if="loading" size="24px" class="absolute" :class="`${classSpinner} text-${$attrs.color}`"  />
       <slot></slot>
+      <q-popup-proxy v-if="popupProxy">
+        <q-banner style="max-width: 380px">
+          <div class="text-subtitle1">
+            <q-icon size="20px" name="fa-solid fa-circle-question" color="green-5" />
+            <span class="q-pl-sm text-center" v-html="popupProxyTitle"></span>
+          </div>
+          <q-separator class="q-mb-md" />
+          <div class="flex items-center justify-between q-gutter-x-md" style="min-width: 265px">
+            <div>
+              <StandardButton outline buttonClass="brand-button-negative" label="Cancelar" v-close-popup />
+            </div>
+            <div>
+              <StandardButton buttonClass="brand-button-positive" label="Confirmar" @click="confirm" v-close-popup />
+            </div>
+          </div>
+        </q-banner>
+      </q-popup-proxy>
+      <q-tooltip v-if="!!tooltip" :class="$attrs.disable ? 'bg-grey' : `bg-${$attrs.color}`" transition-show="rotate"
+        transition-hide="rotate" ref="tooltip">
+        {{ tooltip }}
+      </q-tooltip>
     </q-btn>
   </div>
 </template>
@@ -22,6 +43,9 @@
 defineOptions({
   name: "StandardButton",
 });
+
+//Usado na confirmação do popupProxy
+const emits = defineEmits(['confirm']);
 
 const props = defineProps({
   label: String,
@@ -60,6 +84,24 @@ const props = defineProps({
     type: Boolean,
     default: true,
   },
+  popupProxy: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  popupProxyTitle: {
+    type: String,
+    required: false,
+    default: 'Deseja realizar essa ação?'
+  },
+  tooltip: {
+    type: String,
+    required: false
+  },
 });
+
+const confirm = () => {
+  emits('confirm');
+}
 
 </script>
