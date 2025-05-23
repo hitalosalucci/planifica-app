1efb3251d2a149a0db636bb2dc51bb38641b42d8
Hitalo Salucci <hitalo.salucci@hotmail.com>
2025-05-23 09:21:10 -0300

    FEAT: Add icon favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Planifica App</title>
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
+
+    <link rel="icon" href="{{ asset('images/brand/planifica-icon.svg') }}" type="image/png">
+
+    <link rel="shortcut icon" href="{{ asset('images/brand/planifica-icon.svg') }}">
+    <link rel="apple-touch-icon" href="{{ asset('images/brand/planifica-icon.svg') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('images/brand/planifica-icon.svg') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/brand/planifica-icon.svg') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/brand/planifica-icon.svg') }}">
+
 </head>
 <body>
     <div id="app"></div>
